295abfdc058ba670da03b4c8a4ceb63368c8bcc7
Colin Lienard <colin.lienard87@gmail.com>
2024-12-27 11:58:14 +0100

    feat(raycast): add custom scripts

---
+++ dot_config/raycast/custom-scripts/executable_dev-setup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Dev setup
+# @raycast.mode compact
+
+# Optional parameters:
+# @raycast.icon 🖥️
+
+# Documentation:
+# @raycast.description Open Arc, Zed, Ghostty, Spotify and Discord.
+# @raycast.author colinlienard
+# @raycast.authorURL https://raycast.com/colinlienard
+
+open -a "Arc"
+open -a "Zed"
+open -a "Ghostty"
+open -a "Spotify"
+open -a "Discord"
---
+++ dot_config/raycast/custom-scripts/executable_work-setup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Work setup
+# @raycast.mode compact
+
+# Optional parameters:
+# @raycast.icon 💼
+
+# Documentation:
+# @raycast.description Open Arc, Zed, Ghostty, Linear, Slack, and Spotify.
+# @raycast.author colinlienard
+# @raycast.authorURL https://raycast.com/colinlienard
+
+open -a "Arc"
+open -a "Zed"
+open -a "Ghostty"
+open -a "Linear"
+open -a "Slack"
+open -a "Spotify"
