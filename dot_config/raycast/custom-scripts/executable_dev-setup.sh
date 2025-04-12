47a22136abbf7ceb0eff8db54056b6e9169a93b2
Colin Lienard <colin.lienard87@gmail.com>
2025-04-12 12:52:01 +0200

    update

--- dot_config/raycast/custom-scripts/executable_dev-setup.sh
+++ dot_config/raycast/custom-scripts/executable_dev-setup.sh
@@ -2,19 +2,32 @@
 
 # Required parameters:
 # @raycast.schemaVersion 1
-# @raycast.title Dev setup
+# @raycast.title Dev Setup
 # @raycast.mode compact
 
 # Optional parameters:
-# @raycast.icon 🖥️
+# @raycast.icon 🪐
 
 # Documentation:
 # @raycast.description Open Arc, Zed, Ghostty, Spotify and Discord.
 # @raycast.author colinlienard
 # @raycast.authorURL https://raycast.com/colinlienard
 
-open -a "Arc"
-open -a "Zed"
-open -a "Ghostty"
-open -a "Spotify"
-open -a "Discord"
+open -a Arc
+osascript -e 'tell application "Arc" to activate'
+open -g raycast://extensions/raycast/window-management/maximize
+
+open -a Zed
+osascript -e 'tell application "Zed" to activate'
+open -g raycast://extensions/raycast/window-management/maximize
+
+open -a Ghostty
+osascript -e 'tell application "Ghostty" to activate'
+open -g raycast://extensions/raycast/window-management/maximize
+
+open -a Spotify
+osascript -e 'tell application "Spotify" to activate'
+open -g raycast://extensions/raycast/window-management/maximize
+open -g raycast://extensions/raycast/window-management/next-display
+
+open -a Discord
--- dot_config/raycast/custom-scripts/executable_work-setup.sh
+++ dot_config/raycast/custom-scripts/executable_work-setup.sh
@@ -2,7 +2,7 @@
 
 # Required parameters:
 # @raycast.schemaVersion 1
-# @raycast.title Work setup
+# @raycast.title Work Setup
 # @raycast.mode compact
 
 # Optional parameters:
@@ -13,9 +13,28 @@
 # @raycast.author colinlienard
 # @raycast.authorURL https://raycast.com/colinlienard
 
-open -a "Arc"
-open -a "Zed"
-open -a "Ghostty"
-open -a "Linear"
-open -a "Slack"
-open -a "Spotify"
+open -a Arc
+osascript -e 'tell application "Arc" to activate'
+open -g raycast://extensions/raycast/window-management/maximize
+
+zed ~/ms/mobsuccess-front
+osascript -e 'tell application "Zed" to activate'
+open -g raycast://extensions/raycast/window-management/maximize
+
+open -a Ghostty
+osascript -e 'tell application "Ghostty" to activate'
+open -g raycast://extensions/raycast/window-management/maximize
+
+open -a Linear
+osascript -e 'tell application "Linear" to activate'
+open -g raycast://extensions/raycast/window-management/maximize
+
+open -a Slack
+osascript -e 'tell application "Slack" to activate'
+open -g raycast://extensions/raycast/window-management/maximize
+open -g raycast://extensions/raycast/window-management/next-display
+
+open -a Spotify
+osascript -e 'tell application "Spotify" to activate'
+open -g raycast://extensions/raycast/window-management/maximize
+open -g raycast://extensions/raycast/window-management/next-display
