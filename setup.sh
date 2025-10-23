d5ed4bf308575a156e3c438046fdb067c579bc16
Colin Lienard <colin.lienard87@gmail.com>
2025-10-23 21:51:06 +0200

    feat: add setup.sh and update zed

---
+++ setup.sh
@@ -0,0 +1,14 @@
+# starship
+curl -sS https://starship.rs/install.sh | sh
+
+# fnm
+curl -fsSL https://fnm.vercel.app/install | bash
+
+# bun
+curl -fsSL https://bun.sh/install | bash
+
+# lazygit
+## linux
+sudo apt install lazygit
+## macos
+# brew install lazygit
